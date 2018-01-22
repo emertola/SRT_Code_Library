39d4687af1bd73d9b817d42063c3f95fe868ada7
Erwin Mertola <emertola@gmail.com>
2018-01-22 12:29:25 +0800

    Updated fillInputFieldsByButtonClick

--- fillInputFieldsByButtonClick.js
+++ fillInputFieldsByButtonClick.js
@@ -1,56 +1,78 @@
 
-var newElem = document.createElement("dynamicbutton");
-var elemText = document.createTextNode("Click to run the function");
-newElem.appendChild(elemText);
-var bodyTag = document.getElementsByTagName("body")[0];
-bodyTag.insertBefore(newElem, bodyTag.childNodes[0])
+var parentElement = document.getElementsByTagName("body")[0];
+var childElement = document.getElementsByTagName("dynamicbutton");
 
 
-var getNewElem = document.getElementsByTagName("dynamicbutton")[0];
-getNewElem.setAttribute("onClick", "runFunc()");
-getNewElem.setAttribute("style", "background-color: teal; color: white; padding: 5px; margin-top: 10px; cursor: pointer; position: relative; top: 10px; left: 10px; z-index: 9999;")
+if(childElement && childElement.length > 0) {
+	parentElement.removeChild(childElement[0]);
+	fillInputfieldsByButtonClick();
+} else {
+	fillInputfieldsByButtonClick();
+}
+
+
+function fillInputfieldsByButtonClick() {
+
+	var newElem = document.createElement("dynamicbutton");
+	var elemText = document.createTextNode("Click to run the function");
+	newElem.appendChild(elemText);
+	var bodyTag = document.getElementsByTagName("body")[0];
+	bodyTag.insertBefore(newElem, bodyTag.childNodes[0])
+
+
+	var getNewElem = document.getElementsByTagName("dynamicbutton")[0];
+	getNewElem.setAttribute("onClick", "runFunc()");
+	getNewElem.setAttribute("style", "background-color: teal; color: white; padding: 5px; margin-top: 10px; cursor: pointer; position: relative; top: 10px; left: 10px; z-index: 9999;")
+
+
+}
+
 
 function runFunc() {
-	var pageID = prompt('Please enter the page ID')
-	console.log("You've entered: " + pageID)
+		var pageID = prompt('Please enter the page ID')
 
-	var stage = document.getElementById("stage" + pageID).querySelectorAll("input, select, textarea")
-	console.log(stage)
+		if(pageID) {
+			console.log("You've entered: " + pageID)
 
-	let counter = 1;
+			var stage = document.getElementById("stage" + pageID).querySelectorAll("input, select, textarea")
+			// console.log(stage)
 
-	const elemDisabled = (elem) => elem.disabled ? true : false;
-	const elemReadonly = (elem) => elem.readOnly ? true : false;
+			let counter = 1;
 
-	for(var i = 0; i < stage.length; i++) {
-		switch(stage[i].type) {
-			case "text":
-			case "textarea": {
-				if(!elemDisabled(stage[i]) && !elemReadonly(stage[i]) && stage[i].value === "") {
-					stage[i].value = "fd" + counter;
-					counter += 1;
-				}
-			}
+			const elemDisabled = (elem) => elem.disabled ? true : false;
+			const elemReadonly = (elem) => elem.readOnly ? true : false;
 
-			case "radio": {
-				var rdName = document.getElementsByName(stage[i].name);
-				rdName[0].checked = true;
-			}
+			for(var i = 0; i < stage.length; i++) {
+				switch(stage[i].type) {
+					case "text":
+					case "textarea": {
+						if(!elemDisabled(stage[i]) && !elemReadonly(stage[i]) && stage[i].value === "") {
+							stage[i].value = "fd" + counter;
+							counter += 1;
+						}
+					}
 
-			case "checkbox" : {
-				if(!elemDisabled(stage[i]) && !elemReadonly(stage[i])) {
-					stage[i].checked = true;
-				}
-			}
+					case "radio": {
+						var rdName = document.getElementsByName(stage[i].name);
+						rdName[0].checked = true;
+					}
 
-			case "select-one":
-			case "select-multiple": {
-				if(!elemDisabled(stage[i]) && !elemReadonly(stage[i]) && stage[i].value === "") {
-					const selIndex = stage[i].selectedIndex + 1;
-					stage[i].selectedIndex = selIndex;
+					case "checkbox" : {
+						if(!elemDisabled(stage[i]) && !elemReadonly(stage[i])) {
+							stage[i].checked = true;
+						}
+					}
+
+					case "select-one":
+					case "select-multiple": {
+						if(!elemDisabled(stage[i]) && !elemReadonly(stage[i]) && stage[i].value === "") {
+							const selIndex = stage[i].selectedIndex + 1;
+							stage[i].selectedIndex = selIndex;
+						}
+					}
 				}
 			}
 		}
-	}
+		
 
-}
+	}
