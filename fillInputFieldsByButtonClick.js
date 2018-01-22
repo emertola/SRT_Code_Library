e60eaf9c3d2ecefb33614b80d10a0edd4f244d79
Erwin Mertola <emertola@gmail.com>
2018-01-22 15:06:17 +0800

    Updated fillInputFieldsByButtonClick

--- fillInputFieldsByButtonClick.js
+++ fillInputFieldsByButtonClick.js
@@ -24,55 +24,55 @@ function fillInputfieldsByButtonClick() {
 	getNewElem.setAttribute("onClick", "runFunc()");
 	getNewElem.setAttribute("style", "background-color: teal; color: white; padding: 5px; margin-top: 10px; cursor: pointer; position: relative; top: 10px; left: 10px; z-index: 9999;")
 
+	return true;
+
 
 }
 
 
 function runFunc() {
-		var pageID = prompt('Please enter the page ID')
+	var pageID = prompt('Please enter the page ID')
 
-		if(pageID) {
-			console.log("You've entered: " + pageID)
+	if(pageID) {
 
-			var stage = document.getElementById("stage" + pageID).querySelectorAll("input, select, textarea")
-			// console.log(stage)
+		var stage = document.getElementById("stage" + pageID).querySelectorAll("input, select, textarea")
 
-			let counter = 1;
+		let counter = 1;
 
-			const elemDisabled = (elem) => elem.disabled ? true : false;
-			const elemReadonly = (elem) => elem.readOnly ? true : false;
+		const elemDisabled = (elem) => elem.disabled ? true : false;
+		const elemReadonly = (elem) => elem.readOnly ? true : false;
 
-			for(var i = 0; i < stage.length; i++) {
-				switch(stage[i].type) {
-					case "text":
-					case "textarea": {
-						if(!elemDisabled(stage[i]) && !elemReadonly(stage[i]) && stage[i].value === "") {
-							stage[i].value = "fd" + counter;
-							counter += 1;
-						}
+		for(var i = 0; i < stage.length; i++) {
+			switch(stage[i].type) {
+				case "text":
+				case "textarea": {
+					if(!elemDisabled(stage[i]) && !elemReadonly(stage[i]) && stage[i].value === "") {
+						stage[i].value = "fd" + counter;
+						counter += 1;
 					}
+				}
 
-					case "radio": {
-						var rdName = document.getElementsByName(stage[i].name);
-						rdName[0].checked = true;
+				case "radio": {
+					var rdName = document.getElementsByName(stage[i].name);
+					rdName[0].checked = true;
+				}
+
+				case "checkbox" : {
+					if(!elemDisabled(stage[i]) && !elemReadonly(stage[i])) {
+						stage[i].checked = true;
 					}
+				}
 
-					case "checkbox" : {
-						if(!elemDisabled(stage[i]) && !elemReadonly(stage[i])) {
-							stage[i].checked = true;
-						}
-					}
-
-					case "select-one":
-					case "select-multiple": {
-						if(!elemDisabled(stage[i]) && !elemReadonly(stage[i]) && stage[i].value === "") {
-							const selIndex = stage[i].selectedIndex + 1;
-							stage[i].selectedIndex = selIndex;
-						}
+				case "select-one":
+				case "select-multiple": {
+					if(!elemDisabled(stage[i]) && !elemReadonly(stage[i]) && stage[i].value === "") {
+						const selIndex = stage[i].selectedIndex + 1;
+						stage[i].selectedIndex = selIndex;
 					}
 				}
 			}
 		}
-		
-
 	}
+	
+
+}
